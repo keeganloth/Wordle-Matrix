c35cf544ca12e12a56b4c526c15cfe1deff9b773
keegan.loth <keegan.loth@MU-C1-19.mullen.pvt.k12.co.us>
2026-09-25 12:17:09 -0600

    12:16 second version putting while loop and word working need to put the word into the array

--- Wordle-Matrix/src/Wordle.java
+++ Wordle-Matrix/src/Wordle.java
@@ -11,16 +11,16 @@ static String [][] board = {{" "," "," "," "," "}, {" "," "," "," "," "}, {" ","
 public static void main(String[] args)
 
 {
+	System.out.println(randomWord());
+	while (stillPlaying) {
+	displayBoard();
 
 
-displayBoard();
-
-System.out.println(randomWord());
 
 System.out.println(askForWord());
 
 wordIntoArray();
-
+	}
 }
 
 
@@ -84,31 +84,31 @@ private static void displayBoard()
 
 {
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[0][0] + " | | "+ board[0][1] +" | | "+ board[0][2] +" | | "+ board[0][3] +" | | "+ board[0][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[1][0] + " | | "+ board[1][1] +" | | "+ board[1][2] +" | | "+ board[1][3] +" | | "+ board[1][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[2][0] + " | | "+ board[2][1] +" | | "+ board[2][2] +" | | "+ board[2][3] +" | | "+ board[2][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[3][0] + " | | "+ board[3][1] +" | | "+ board[3][2] +" | | "+ board[3][3] +" | | "+ board[3][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[4][0] + " | | "+ board[4][1] +" | | "+ board[4][2] +" | | "+ board[4][3] +" | | "+ board[4][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 System.out.println("| " + board[5][0] + " | | "+ board[5][1] +" | | "+ board[5][2] +" | | "+ board[5][3] +" | | "+ board[5][4] +" | ");
 
-System.out.println(" --- --- --- --- ---");
+System.out.println(" ---   ---   ---   ---   ---");
 
 }
 
